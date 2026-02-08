ed8d63c12fbb82db262a8e91753bbe26a406ddbe
AdrianRCdwec <arodriguezc24@fpcoruna.afundacion.org>
2026-02-08 18:40:51 +0100

    Postman correcto

--- SEMANA3/streaming/urls.py
+++ SEMANA3/streaming/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PeliculaViewSet, CategoriaViewSet
+
+from .views import (
+    PeliculaViewSet,
+    CategoriaViewSet,
+    EtiquetaViewSet,
+    ResenaViewSet,
+    PerfilViewSet,
+)
 
 router = DefaultRouter()
 router.register(r'peliculas', PeliculaViewSet, basename='pelicula')
 router.register(r'categorias', CategoriaViewSet, basename='categoria')
+router.register(r'etiquetas', EtiquetaViewSet, basename='etiqueta')
+router.register(r'resenas', ResenaViewSet, basename='resena')
+router.register(r'perfiles', PerfilViewSet, basename='perfil')
 
 urlpatterns = [
     path('', include(router.urls)),
